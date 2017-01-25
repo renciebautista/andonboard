b8433fa1c81712a69529c14d0e2536c7fdc70116
Rencie Bautista <rencie.bautista@yahoo.com>
2017-01-25 11:21:35 +0800

    add logs table

---
+++ database/migrations/2017_01_25_031348_create_logs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('line_id')->unsigned();
+            $table->foreign('line_id')->references('id')->on('lines');
+            $table->integer('shift_id')->unsigned();
+            $table->foreign('shift_id')->references('id')->on('shifts');
+            $table->integer('target');
+            $table->integer('actual');
+            $table->date('run_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('logs');
+    }
+}
